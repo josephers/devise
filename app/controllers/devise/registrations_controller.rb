f76c33523b1635f618570e8eebf39ed53f8079a4
josephers <joseph@roundbox.ca>
2012-02-10 17:43:44 -0500

    Update app/controllers/devise/registrations_controller.rb

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -12,23 +12,24 @@ class Devise::RegistrationsController < DeviseController
   # POST /resource
   def create
     build_resource
-    binding.remote_pry
     if existing_resource = resource_class.find_by_email(resource.email) and existing_resource.valid_password?(resource.password)
-        sign_in_and_redirect resource, event: :authentication and return
-    end
-    if resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
         sign_in(resource_name, resource)
         respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
     else
-      clean_up_passwords resource
-      respond_with resource
+      if resource.save
+        if resource.active_for_authentication?
+          set_flash_message :notice, :signed_up if is_navigational_format?
+          sign_in(resource_name, resource)
+          respond_with resource, :location => after_sign_up_path_for(resource)
+        else
+          set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
+          expire_session_data_after_sign_in!
+          respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords resource
+        respond_with resource
+      end
     end
   end
 
